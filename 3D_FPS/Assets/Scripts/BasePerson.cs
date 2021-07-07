9ef101f313b51954abbf581c317c9a26c14360ec
KID <50666207+KID421@users.noreply.github.com>
2021-07-07 21:47:32 +0800

    添加基底人物與角色控制類別

---
+++ 3D_FPS/Assets/Scripts/BasePerson.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class BasePerson : MonoBehaviour
+{
+    #region 欄位
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 10;
+    [Header("跳躍高度"), Range(0, 1000)]
+    public float jump = 100;
+    [Header("血量"), Range(0, 1000)]
+    public float hp = 100;
+    [Header("攻擊力"), Range(0, 100)]
+    public float attack = 10;
+
+    /// <summary>
+    /// 血量最大值
+    /// </summary>
+    private float hpMax;
+    private Animator ani;
+    private Rigidbody rig;
+    private AudioSource aud;
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        #region 取得元件
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+        aud = GetComponent<AudioSource>();
+        #endregion
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 移動
+    /// </summary>
+    /// <param name="movePosition">要移動的座標資訊</param>
+    public void Move(Vector3 movePosition)
+    {
+        // 剛體.移動座標(物件座標 + 移動座標 * 速度)
+        rig.MovePosition(transform.position + movePosition * speed);
+    }
+
+    [Header("旋轉速度"), Range(0, 1000)]
+    public float turn = 5;
+
+    /// <summary>
+    /// 旋轉
+    /// </summary>
+    /// <param name="turnValue">要旋轉的值</param>
+    public void Turn(Vector3 turnValue)
+    {
+        // 變形.旋轉(三維向量 * 旋轉速度)
+        transform.Rotate(turnValue * turn);
+    }
+    #endregion
+}
---
+++ 3D_FPS/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 玩家控制類別：玩家滑鼠、鍵盤的輸入資訊以及跟 Base Person 溝通
+/// </summary>
+public class ControllerPlayer : MonoBehaviour
+{
+    #region 欄位
+    /// <summary>
+    /// 人物基底類別
+    /// </summary>
+    private BasePerson basePerson;
+    /// <summary>
+    /// 要移動的座標資訊
+    /// </summary>
+    private Vector3 v3Move;
+    /// <summary>
+    /// 要旋轉的值
+    /// </summary>
+    private Vector3 v3Turn;
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        basePerson = GetComponent<BasePerson>();
+    }
+
+    private void Update()
+    {
+        GetMoveInput();
+        GetTurnInput();
+    }
+
+    // 固定更新事件：50 FPS 物理行為在此事件內執行
+    private void FixedUpdate()
+    {
+        basePerson.Move(v3Move);
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 取得移動輸入資訊
+    /// </summary>
+    private void GetMoveInput()
+    {
+        float h = Input.GetAxis("Horizontal");  // 水平值 A -1，D 1
+        float v = Input.GetAxis("Vertical");    // 垂直值 S -1，W 1
+        v3Move.x = h;                           // 左右為 X 軸
+        v3Move.z = v;                           // 前後為 Z 軸
+    }
+
+    /// <summary>
+    /// 取得旋轉輸入資訊
+    /// </summary>
+    private void GetTurnInput()
+    {
+        float mouseX = Input.GetAxis("Mouse X");    // 取得滑鼠 X 值
+        float mouseY = Input.GetAxis("Mouse Y");    // 取得滑鼠 Y 值
+        v3Turn.x = mouseY;                          // 物件 X 軸對應滑鼠 Y
+        v3Turn.y = mouseX;                          // 物件 Y 軸對應滑鼠 X
+    }
+    #endregion
+}
